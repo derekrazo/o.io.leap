383ff3805ed06ff3488dedbb56f9ebd79734983a
Derek Razo <derekrazo@Dereks-MacBook-Pro-2.local>
2013-06-13 11:22:06 -0700

    commiting full  xcode project

---
+++ JohnMacCallumsBeard/simplemax_osc.h
@@ -0,0 +1,14 @@
+//
+//  simplemax_osc.h
+//  simplemax
+//
+//  Created by Derek Razo on 4/19/13.
+//
+//
+
+#ifndef __simplemax__simplemax_osc__
+#define __simplemax__simplemax_osc__
+
+#include <iostream>
+
+#endif /* defined(__simplemax__simplemax_osc__) */
